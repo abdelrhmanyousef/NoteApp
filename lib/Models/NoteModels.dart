163cc376a0e3d17500030fcd006140c4ae12c064
abdelrhmanyousef <abdo.yusiif@gmail.com>
2023-07-29 07:30:53 +0200

    create type adapter for note

--- lib/Models/NoteModels.dart
+++ lib/Models/NoteModels.dart
@@ -1,8 +1,18 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'NoteModels.g.dart';
+
+@HiveType(typeId: 1)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String Titel;
+  @HiveField(1)
   final String SubTitel;
+  @HiveField(2)
   final String data;
+  @HiveField(3)
   final int color;
+
   NoteModel({
     required this.Titel,
     required this.SubTitel,
---
+++ lib/Models/NoteModels.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'NoteModels.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class NoteModelAdapter extends TypeAdapter<NoteModel> {
+  @override
+  final int typeId = 1;
+
+  @override
+  NoteModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return NoteModel(
+      Titel: fields[0] as String,
+      SubTitel: fields[1] as String,
+      data: fields[2] as String,
+      color: fields[3] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, NoteModel obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.Titel)
+      ..writeByte(1)
+      ..write(obj.SubTitel)
+      ..writeByte(2)
+      ..write(obj.data)
+      ..writeByte(3)
+      ..write(obj.color);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is NoteModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
